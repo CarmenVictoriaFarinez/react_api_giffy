f9a10fbf7c7a7831c32afe036e041e91ee29ea7e
DESKTOP-UHQI77P\vmc <cfarinez@vmcsubastas.com>
2021-05-12 19:27:28 -0500

    save project

---
+++ src/components/Gif.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Gif ({id, title, url}){
+    return (
+        <div>
+            <h4 >{title}</h4>
+            <img src={url} alt={title}/>
+        </div>
+    )
+}
